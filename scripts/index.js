bd7bf7a39a3f44df6f66bceaf0c4fea4dc4ddf0d
JonatanNunes <jonyfuel@gmail.com>
2024-12-12 18:57:54 -0300

    0.3

--- scripts/index.js
+++ scripts/index.js
@@ -1,156 +1,128 @@
+// Elementos del DOM
+const startGameButton = document.getElementById('startGameButton');
+const menu = document.getElementById('menu');
+const miniBattleButton = document.getElementById('miniBattleButton');
+const catanButton = document.getElementById('catanButton');
+const exitButton = document.getElementById('exitButton');
+const gameContainer = document.getElementById('gameContainer');
+const gameBoard = document.getElementById('gameBoard');
+const statusMessage = document.getElementById('statusMessage');
+const coordinateInput = document.getElementById('coordinate');
+const submitCoordinateButton = document.getElementById('submitCoordinate');
 
-const showMenu = () => {
-    let menu = '';
+let playerArmy = [];
+let computerArmy = [];
+let boardSize = 4;
+let armySize = 3;
+let currentTurn = 'player'; // 'player' o 'computer'
+let rounds = 1;
 
-    do {
-        menu = prompt('Qué desea hacer: \n 1. Jugar mini batalla naval \n 2. Jugar Catan \n 3. Salir');
+startGameButton.addEventListener('click', () => {
+  menu.classList.remove('hidden');
+  startGameButton.classList.add('hidden');
+});
 
-        switch (menu) {
-            case '1':
-                tinyNavalBattle();
-                break;
-            case '2':
-                alert ('No disponible. Intente más tarde!');
-                break;
-            case '3':
-                alert ('Gracias por usar la app. Si quiere volver a usarla presione el botón o refresque la página');
-                break;
-        }
+// Mostrar el menú con opciones
+miniBattleButton.addEventListener('click', () => {
+  menu.classList.add('hidden');
+  gameContainer.classList.remove('hidden');
+  startMiniNavalBattle();
+});
 
+exitButton.addEventListener('click', () => {
+  window.location.reload();
+});
 
-    } while (menu !== '3');
-};
+submitCoordinateButton.addEventListener('click', () => {
+  const coordinate = coordinateInput.value.trim().toUpperCase();
+  if (coordinate) {
+    handlePlayerTurn(coordinate);
+  } else {
+    alert('Por favor ingresa una coordenada válida.');
+  }
+});
 
-const convertLetterToNumber = (letter) => {
-    return letter?.toLowerCase().charCodeAt(0) - 97;
-};
+function startMiniNavalBattle() {
+  alert('¡Bienvenido a la batalla naval miniatura! \n Para jugar deberás ingresar las coordenadas de tus barcos.');
 
-const convertNumberToLetter = (number) => {
-    return String.fromCharCode(number + 97).toUpperCase();
+  // Crear las flotas de barcos
+  playerArmy = createArmy(armySize, boardSize);
+  computerArmy = createArmy(armySize, boardSize, true);
+
+  // Mostrar el tablero
+  showArmy(playerArmy, boardSize);
+
+  // Actualizar mensaje de estado
+  statusMessage.innerText = `Es tu turno, ingresa una coordenada para atacar.`;
 }
 
-
-const asignValidCoordinate = (boardSize, currentValues) => {
-    const maxLetterRange = convertNumberToLetter(boardSize - 1);
-
-    const validLetters = Array.from({ length: boardSize }, (_, i) => convertNumberToLetter(i));
-    const validNumbers = Array.from({ length: boardSize }, (_, i) => i);
-
-    const isValidInput = (input) => input.length === 2 && validLetters.includes(input[0].toUpperCase()) && validNumbers.includes(parseInt(input[1]));
-    const isRepeated = (input) => currentValues.some((coordinate) => coordinate.x === convertLetterToNumber(input[0]) && coordinate.y === parseInt(input[1]));
-
-    let input = '';
-    
-    do {
-        input = prompt(`Ingrese una coordenada válida entre A0 y ${maxLetterRange}${boardSize - 1}`);
-        if (!isValidInput(input)) {
-            alert ('Coordenada inválida. Intente nuevamente');
-        }
-
-        if (isRepeated(input)) {
-            alert ('Coordenada repetida. Intente nuevamente');
-        }
-
-    } while (!isValidInput(input) || isRepeated(input));
-
-    return {x: convertLetterToNumber(input[0]), y: parseInt(input[1])};
+function createArmy(armySize, boardSize, IAselection = false) {
+  const army = [];
+  for (let i = 0; i < armySize; i++) {
+    const coordinate = IAselection ? { x: Math.floor(Math.random() * boardSize), y: Math.floor(Math.random() * boardSize) } : asignValidCoordinate(boardSize, army);
+    army.push(coordinate);
+  }
+  return army;
 }
 
-const createArmy = (armySize, boardSize, IAselection) => {
-    const army = [];
-    for (let i = 0; i < armySize ; i++) {
-       const coordinate = IAselection ? {x: Math.floor(Math.random() * boardSize), y: Math.floor(Math.random()*boardSize)} : asignValidCoordinate(boardSize, army);
-         
-       army.push(coordinate);
+function asignValidCoordinate(boardSize, currentValues) {
+  const maxLetterRange = convertNumberToLetter(boardSize - 1);
+
+  const validLetters = Array.from({ length: boardSize }, (_, i) => convertNumberToLetter(i));
+  const validNumbers = Array.from({ length: boardSize }, (_, i) => i);
+
+  const isValidInput = (input) => input.length === 2 && validLetters.includes(input[0].toUpperCase()) && validNumbers.includes(parseInt(input[1]));
+  const isRepeated = (input) => currentValues.some((coordinate) => coordinate.x === convertLetterToNumber(input[0]) && coordinate.y === parseInt(input[1]));
+
+  let input = '';
+
+  do {
+    input = prompt(`Ingrese una coordenada válida entre A0 y ${maxLetterRange}${boardSize - 1}`);
+    if (!isValidInput(input)) {
+      alert('Coordenada inválida. Intente nuevamente');
     }
 
-    return army;
-};
-
-const showArmy = (army, boardSize) => {
-    let armyString = '';
-
-    armyString += ' \u00a0 \u00a0 \u00a0 \u00a0' + Array(boardSize).fill().map((_, i) => convertNumberToLetter(i)).join('\u00a0 \u00a0') + '\n';
-
-    Array(boardSize).fill('').forEach((_, posY) => {
-        let row = `\u00a0 ${posY} \u00a0`;
-      Array(boardSize).fill('').forEach((_, posX) => {
-        row += army.some((coordinate) => coordinate.x === posX && coordinate.y === posY) ? '\u00a0 X \u00a0' : '\u00a0 \u00a0 \u00a0';
-      });
-        armyString += row + '\n';
-    });
-
-    alert(armyString);
-}
-
-const IANewCoordinate = (boardSize, usedCoordinates) => {
-    let coordinate = {x: Math.floor(Math.random() * boardSize), y: Math.floor(Math.random()*boardSize)};
-
-    while (usedCoordinates.some((usedCoordinate) => usedCoordinate.x === coordinate.x && usedCoordinate.y === coordinate.y)) {
-        coordinate = {x: Math.floor(Math.random() * boardSize), y: Math.floor(Math.random()*boardSize)};
+    if (isRepeated(input)) {
+      alert('Coordenada repetida. Intente nuevamente');
     }
 
-    return coordinate;
+  } while (!isValidInput(input) || isRepeated(input));
+
+  return { x: convertLetterToNumber(input[0]), y: parseInt(input[1]) };
 }
 
-const navalGamePlay = (playerArmy, computerArmy, boardSize) => {
-    let playerTurn = true;
-    let rounds = 1;
-    const IAusedCoordinates = [];
-
-    alert(`¡Es momento de jugar! \n Recuerda que el tablero es de ${boardSize}x${boardSize} y que los barcos tienen un solo casillero de dimensiones. \n ¡Buena suerte!`);
-    
-    do {
-        if (playerTurn) {
-            alert('Es tu turno');
-
-            const coordinate = asignValidCoordinate(boardSize, []);
-            
-            if (computerArmy.some((computerCoordinate) => computerCoordinate.x === coordinate.x && computerCoordinate.y === coordinate.y)) {
-                alert('¡Hundiste un barco enemigo!');
-                computerArmy = computerArmy.filter((computerCoordinate) => computerCoordinate.x !== coordinate.x || computerCoordinate.y !== coordinate.y);
-            } else {
-                alert('Agua!! No hay barcos enemigos en esta coordenada');
-            }
-
-            
-        } else {
-            alert('Turno de la IA');
-
-            const coordinate = IANewCoordinate(boardSize, IAusedCoordinates);
-
-            if (playerArmy.some((playerCoordinate) => playerCoordinate.x === coordinate.x && playerCoordinate.y === coordinate.y)) {
-                alert('¡La IA hundió uno de tus barcos!');
-                playerArmy = playerArmy.filter((playerCoordinate) => playerCoordinate.x !== coordinate.x || playerCoordinate.y !== coordinate.y);
-            } else {
-                alert(`${convertNumberToLetter(coordinate.x)}${coordinate.y} ! La IA falló!`);
-            }
-
-            rounds++;
-        }
-        playerTurn = !playerTurn;
-
-} while (playerArmy.length > 0 && computerArmy.length > 0);
-
-if (!playerArmy.length) {
-    alert(`¡Perdiste! La IA hundió todos tus barcos en ${rounds} rondas`);
-} else {
-    alert(`¡Ganaste! Hundiste todos los barcos de la IA en ${rounds} rondas`);
-}
+function convertLetterToNumber(letter) {
+  return letter.toLowerCase().charCodeAt(0) - 97;
 }
 
-// JUEGO DE BATALLA NAVAL MINIATURA
-const tinyNavalBattle = () => {
-    const boardSize = 4;
-    const armySize = 3;
-
-    alert('¡Bienvenido a la batalla naval miniatura! \n \n Para jugar deberás ingresar las coordenadas de tus barcos. \n \n Recuerda que en esta versión los barcos tienen un solo casillero de dimensiones. \n \n ¡Buena suerte!');
-
-    let playerArmy = createArmy(armySize, boardSize);
-    let computerArmy = createArmy(armySize, boardSize, true);
-    
-    showArmy(playerArmy, boardSize);
-
-    navalGamePlay(playerArmy, computerArmy, boardSize);
+function convertNumberToLetter(number) {
+  return String.fromCharCode(number + 97).toUpperCase();
 }
 
+function showArmy(army, boardSize) {
+  gameBoard.innerHTML = ''; // Limpiar el tablero anterior
+
+  // Mostrar filas
+  for (let i = 0; i < boardSize; i++) {
+    const row = document.createElement('div');
+    row.classList.add('d-flex');
+    for (let j = 0; j < boardSize; j++) {
+      const cell = document.createElement('div');
+      const hasShip = army.some(coordinate => coordinate.x === j && coordinate.y === i);
+      cell.textContent = hasShip ? 'X' : '';
+      cell.classList.add('cell', 'border', 'p-2');
+      row.appendChild(cell);
+    }
+    gameBoard.appendChild(row);
+  }
+}
+
+function handlePlayerTurn(coordinate) {
+  // Convertir coordenada ingresada por el jugador en un objeto
+  const x = convertLetterToNumber(coordinate[0]);
+  const y = parseInt(coordinate[1]);
+
+  // Verificar si el jugador ha acertado o fallado
+  if (computerArmy.some(coordinate => coordinate.x === x && coordinate.y === y)) {
+    alert('¡Hundiste un barco enemigo!');
+    computerArmy = computerArmy.filter(coordinate => coordinate.x
